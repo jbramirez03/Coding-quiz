7fb728268d21d961a9d671efa9cbf2ac5559a3f2
temptado <jason1287712@gmail.com>
2021-07-02 19:17:22 -0400

    defined questions and answers

--- assets/script.js
+++ assets/script.js
@@ -27,40 +27,36 @@ var timeInterval = setInterval(function () {
 }, 1000);
 }
 
+// function to render questions
 
-countdown();
+
 // an array will hold the questions with the questions themselves inside and object as strings
 // inside the objects the options are in an array because we want to select just one option and not all the options as one string
 
 var questionsAsked = [
     {
-        question: "",
-        options: ["", "", "", ""],
-        answer: ""
+        question: "The condition in an if/else estatement is enclosed within ____.",
+        options: ["quotes", "parentheses", "curly brackets", "square brackets"],
+        answer: "parantheses"
     },
     {
-        question: "",
-        options: ["", "", "", ""],
-        answer: ""
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
     },
     {
-        question: "",
-        options: ["", "", "", ""],
-        answer: ""
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["terminal/bash", "for loops", "javascript", "console log"],
+        answer: "console log"
     },
     {
-        question: "",
-        options: ["", "", "", ""],
-        answer: ""
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["curly brackets", "paranthesis", "quotes", "commas"],
+        answer: "quotes"
     },
     {
-        question: "",
-        options: ["", "", "", ""],
-        answer: ""
+        question: "Arrays in javascript can be used to store ____.",
+        options: ["other arrays", "numbers and strings", "booleans", "all of the above"],
+        answer: "all of the above"
     },
-    {
-        question: "",
-        options: ["", "", "", ""],
-        answer: ""
-    }
 ];
