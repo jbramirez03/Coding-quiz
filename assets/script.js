f86a0c780d9b5bfd1fce4cca98062d20d6fcf7e1
temptado <jason1287712@gmail.com>
2021-07-01 20:03:43 -0400

    added arrays that contain questions

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+// an array will hold the questions with the questions themselves inside and object as strings
+// inside the objects the options are in an array because we want to select just one option and not all the options as one string
+
+var questionsAsked = [
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: ""
+    }
+];
