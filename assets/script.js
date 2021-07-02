e1b0fed0dbd96d575808d30b566c32c9c76035cc
temptado <jason1287712@gmail.com>
2021-07-01 21:31:00 -0400

    added timer in script

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,34 @@
 // referencing html tags with id using queryselector
 var startButton = document.querySelector("#startButton");
-var textBox = document.querySelector(".question-textbox");
+var QuestiontextBox = document.querySelector(".question-textbox");
+var timeCount = document.querySelector(".timer-count");
+var secondsTextbox = document.querySelector(".secondsTextbox");
 
 
+// making a interval function that creates a timer
+function countdown () {
+// timer variable defined
+var timer = 60;
 
+var timeInterval = setInterval(function () {
+    if (timer > 1) {
+        timeCount.textContent = timer;
+        timer--;
+    } else if (timer === 1) {
+        timeCount.textContent = timer;
+        timer--;
+        secondsTextbox.textContent = "Second remaining";
+    } else {
+        timeCount.textContent = "";
+        secondsTextbox.textContent = "";
+        clearInterval(timeInterval);
+    }
+
+}, 1000);
+}
+
+
+countdown();
 // an array will hold the questions with the questions themselves inside and object as strings
 // inside the objects the options are in an array because we want to select just one option and not all the options as one string
 
